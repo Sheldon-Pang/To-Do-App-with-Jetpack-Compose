41c67460520da8719750f92fc3fdbbaf9e2f6957
Sheldon Pang <SheldonLinkedin@yahoo.com>
2022-07-03 19:45:19 -0700

    CRUD Operation - Update Task

--- app/src/main/java/com/example/to_doappwithjetpackcompose/ui/theme/viewmodels/SharedViewModels.kt
+++ app/src/main/java/com/example/to_doappwithjetpackcompose/ui/theme/viewmodels/SharedViewModels.kt
@@ -76,13 +76,25 @@ class SharedViewModels @Inject constructor(
         }
     }
 
+    private fun updateTask() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            repository.updateTask(toDoTask = toDoTask)
+        }
+    }
+
     fun handleDatabaseActions (action: Action) {
         when (action) {
             Action.ADD -> {
                 addTask()
             }
             Action.UPDATE -> {
-
+                updateTask()
             }
             Action.DELETE -> {
 
